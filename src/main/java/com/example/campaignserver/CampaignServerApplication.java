1a99b97a187bc542b761f291a3bcce1b3a1d177e
Jia Le <tohjiale123@gmail.com>
2026-02-17 17:27:53 +0800

    add codebase

---
+++ src/main/java/com/example/campaignserver/CampaignServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.campaignserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampaignServerApplication.class, args);
+    }
+
+}
