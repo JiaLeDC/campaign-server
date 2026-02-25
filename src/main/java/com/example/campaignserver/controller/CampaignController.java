1bd5e4955a4154e09bd37331da0ffe13ef50ea82
Jia Le <tohjiale123@gmail.com>
2026-02-25 17:43:55 +0800

    add codebase

---
+++ src/main/java/com/example/campaignserver/controller/CampaignController.java
@@ -0,0 +1,18 @@
+package com.example.campaignserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("campaign")
+public class CampaignController {
+
+    @GetMapping("/testPrint")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Hello  and Test312");
+    }
+
+}
